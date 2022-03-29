27b5d0b9f9140ca4f70d2d0615ccd30f2deddb09
Devorah Ortega <devorahb.ortega@gmail.com>
2022-03-29 15:01:06 -0500

    Adds show featyre to controller

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,10 @@ class PhotosController < ApplicationController
     photo = Photo.all
     render json: photo
   end
+
+  def show
+    photo_id = params["id"]
+    photo = Photo.find_by(id: photo_id)
+    render json: photo
+  end
 end
